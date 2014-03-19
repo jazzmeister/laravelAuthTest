e51bef7d5d8e0834a3f94b0a962b078140451592
jazzmeister <jason@spiderstudios.co.uk>
2014-03-19 21:45:53 +0000

    Change email

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => 'jasonbishp30@gmail.com', 'name' => 'Auth'),
+	'from' => array('address' => 'your email.com', 'name' => 'Auth'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => 'jasonbishp30@gmail.com',
+	'username' => 'your email.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'jutland25',
+	'password' => 'yourpass',
 
 	/*
 	|--------------------------------------------------------------------------
